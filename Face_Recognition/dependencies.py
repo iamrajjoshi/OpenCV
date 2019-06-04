1e92ad2fa56a0dac76aa65c1b5c5bdf56da98cc7
Raj Joshi <geeky2307@gmail.com>
2019-06-04 18:34:45 -0400

    update

--- Face_Recognition/dependencies.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv #open cv
-import numpy as np #numpy array
-from PIL import Image #python imaging library
-import os #operating system
-import sys #system
-import imutils #helpful opencv funcs
-import time #time
-#from imutils.video import VideoStream #for fps
-from imutils.video import FPS #for fps
