5973b61db63830e61c0a115d4bf87cc7402b4717
Raj Joshi <geeky2307@gmail.com>
2019-06-04 17:31:09 -0400

    update

--- Face_Detection/webcam.py
+++ Face_Detection/webcam.py
@@ -1,24 +1,26 @@
-from imports import *
+import cv2 as cv
+import numpy as np
+import imutils
+import time
+from imutils.video import VideoStream
+from imutils.video import FPS
 
-training_cascade_path = "../Haar Cascade/haarcascade_frontalface_default.xml" #change path to where it is stored
+training_cascade_path = "haarcascade_frontalface_default.xml"
 face_cascade = cv.CascadeClassifier(training_cascade_path)
-video_feed = cv.VideoCapture(0)
-
+video_feed = VideoStream(src=1).start()
 print("[INFO] starting video stream...")
-time.sleep(2.0) #taking in account the lag for the video feed to start
+time.sleep(2.0)
 fps = FPS().start()
 
-while True:
-	trash, frame = video_feed.read() #gets webcam feed
+while ((cv.waitKey(1) & 0xFF) != ord("q")):
+	frame = video_feed.read() #get webcam feed
+	frame = imutils.resize(frame, width=500)
 	grayscale_image = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) #grayscale image
-	
-	num_faces_found = face_cascade.detectMultiScale (grayscale_image, scaleFactor=1.1, minNeighbors=5, minSize = (30, 30)) #detect faces in the grayscale image
-	for (x, y, w, h) in num_faces_found: cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2) #draw a rectangle around the faces
+	num_faces_found = face_cascade.detectMultiScale (grayscale_image, scaleFactor=1.1, minNeighbors=10, minSize = (30, 30)) #detect faces
+	for (x, y, w, h) in num_faces_found: cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2) #draw a rectangle around faces
 	cv.imshow('Video Feed', frame)
 	fps.update()
-	if chr(cv.waitKey(0) & 255) == 'q' : break
 
-video_feed.release() #turn camera off
 fps.stop()
 print ("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
 print ("[INFO] FPS: {:.2f}".format(fps.fps()))
